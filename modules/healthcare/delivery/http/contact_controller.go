0823e7a9458aaa731e2a0f47189aea824e5e1a6b
Prayas Poudel <mrprayaspoudel@gmail.com>
2026-01-02 10:34:39 +0545

    feat: Implement authentication middleware and update controller imports

--- modules/healthcare/delivery/http/contact_controller.go
+++ modules/healthcare/delivery/http/contact_controller.go
@@ -4,9 +4,9 @@ import (
 	"math"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/prayaspoudel/modules/access/middleware"
 	"github.com/prayaspoudel/modules/healthcare/features/address"
 	"github.com/prayaspoudel/modules/healthcare/model"
-	"github.com/prayaspoudel/modules/user/middleware"
 	"github.com/sirupsen/logrus"
 )
 
